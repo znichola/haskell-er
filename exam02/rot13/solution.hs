3f4ce811b4a3f4e0f7d7f233485a00c6c6af9970
Nicholas Zivkovic <niki.zivkovic@hotmail.com>
2024-01-08 17:31:46 +0100

    solving rot 13

--- exam02/rot13/solution.hs
+++ exam02/rot13/solution.hs
@@ -1,2 +1,21 @@
+import Data.Char (chr, isAsciiLower, isAsciiUpper, ord)
+import System.Environment (getArgs)
+
 main :: IO ()
-    print "test"
+main = do
+  args <- getArgs
+  putStrLn $ solve args
+  where
+    solve [] = ""
+    solve (x : xs) = map rotate x
+
+wrap :: Int -> Int -> Int -> Int
+wrap upper lower num
+  | num > upper = num - upper + lower - 1
+  | otherwise = num
+
+rotate :: Char -> Char
+rotate c
+  | isAsciiUpper c = chr (wrap 90 65 (ord c + 13))
+  | isAsciiLower c = chr (wrap 122 97 (ord c + 13))
+  | otherwise = c
