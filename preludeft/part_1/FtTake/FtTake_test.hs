db56a5fc2fd94cf0e43749f45e41f44603be3a03
znichola <niki.zivkovic@hotmail.com>
2024-01-16 12:24:49 +0100

    fixx ftTake tests

--- preludeft/part_1/FtTake/FtTake_test.hs
+++ preludeft/part_1/FtTake/FtTake_test.hs
@@ -2,9 +2,13 @@ import Text.Printf (printf)
 import FtTake (ftTake)
 -- import make the linter shutup
 
+infinite = repeat "42"
+
+string = ['a'..'z']
+
 strings = ["foobar", "   spaces might be hard ?", "123456789", ""]
 
-words = [["hello", "world"], ["the", "heck", "is", "7h1s", "#04"], ["", "", "foo", "bar"]]
+listOfStrings = [["hello", "world"], ["the", "heck", "is", "7h1s", "#04"], ["", "", "foo", "bar"]]
 
 numbers = [[1, 2, 3, 4, 5, 6, 7], [2147483647, -2147483648, 0, 1], []]
 
@@ -13,18 +17,20 @@ floats = [1.333, 2.421, 12.9]
 
 doubles = [0.123, -923.0, 1e10, -42e4]
 
-empties = [[], [], []]
+empties = ["a", [], [], [], [], [], [], []]
 
 takeAmounts :: [Int]
-takeAmounts = [-3, 0, 1, 4, 6, 2147483647, -2147483648]
+takeAmounts = [-3, 0, 1, 4, 6, 4200, -2147483648]
 
 main = do
   printf "%s %s\n" "strings" $ pass $ run strings
-  -- printf "%s %s\n" "words" pass $ run words
+  printf "%s %s\n" "string" $ pass $ run string
+  printf "%s %s\n" "listOfStrings" $ pass $ run listOfStrings
   printf "%s %s\n" "numbers" $ pass $ run numbers
   printf "%s %s\n" "floats" $ pass $ run floats
   printf "%s %s\n" "doubles" $ pass $ run doubles
-  -- printf "%s %s\n" "strings" pass $ run empties
+  printf "%s %s\n" "empties" $ pass $ run empties
+  printf "%s %s\n" "infinite" $ pass $ run infinite
 
 pass v = if v then "Pass" else "Fail"
 
