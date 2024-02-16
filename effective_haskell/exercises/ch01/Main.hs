8eb47afe5fe6d160b96b2d4a868bcda719e03df2
Nicholas Zivkovic <znichola@student.42lausanne.ch>
2024-02-16 12:16:08 +0100

    work on ch01

--- effective_haskell/exercises/ch01/Main.hs
+++ effective_haskell/exercises/ch01/Main.hs
@@ -1,3 +1,31 @@
 module Main where
 
-  main = print "Hello world!"
+main = print greeting
+
+salutation = "Hello"
+
+entity = "World"
+
+greeting = salutation <> " " <> entity
+
+makeGreeting sal ent = sal <> " " <> ent
+
+-- enthusiasticGreeting sal name = makeGreeting (sal <> "!") name
+-- reducing the above to what's below is called eta reduction or n-reduction
+enthusiasticGreeting sal = makeGreeting (sal <> "!")
+
+-- using partial application to make new functions
+-- we use parens to turn infix functions into regular functions
+half = (/ 2)
+
+twoOver = (2 /)
+
+-- we can also use ` to make a function infix
+--  usefull for partially applying a function with it's second parameter
+greetJane = (`makeGreeting` "Jane")
+
+flip' func arg1 arg2 = func arg2 arg1
+
+floow a b = a <> "->" <> b
+
+sayThree a b c = a <> b <> c
